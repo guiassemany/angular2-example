0654a709e6ac373b6dedcf9d6cd3313c2cbb9a9e
Guilherme Assemany <guiassemany@users.noreply.github.com>
2016-10-01 21:44:33 -0300

    Finished Angular 2 Example App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Article} from "./article/article.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    articles: Article[];
+
+    constructor() {
+        this.articles = [
+            new Article('Angular 2', 'http://angular.io', 3),
+            new Article('Assemany', 'http://assemany.com', 2),
+            new Article('Blog Assemany', 'http://blog.assemany.com', 1),
+        ];
+    }
+
+    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+        console.log(`Adding Article: ${title.value} and link ${link.value}`);
+        this.articles.push(new Article(title.value, link.value, 0));
+        title.value = '';
+        link.value = '';
+        return false;
+    }
+
+    sortedArticles(): Article[] {
+        return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+    }
 }
